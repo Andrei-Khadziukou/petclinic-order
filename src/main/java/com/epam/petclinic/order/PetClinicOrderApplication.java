945844f4db4ef90b971505469e67c51f0ba66410
Andrei-Khadziukou <khadziukou_andrei@bk.ru>
2017-04-12 15:50:31 +0300

    Initiate repository and configure connection to Eureka

---
+++ src/main/java/com/epam/petclinic/order/PetClinicOrderApplication.java
@@ -0,0 +1,14 @@
+package com.epam.petclinic.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PetClinicOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicOrderApplication.class, args);
+	}
+}
