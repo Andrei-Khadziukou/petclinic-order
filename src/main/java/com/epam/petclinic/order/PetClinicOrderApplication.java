c3d30c48ddecd054123b28f1238a52cae88f85c5
Andrei-Khadziukou <Andrei_Khadziukou@epam.com>
2017-04-17 02:56:33 +0300

    Spring Boot configuration was adjusted. Code formatting was corrected.

--- src/main/java/com/epam/petclinic/order/PetClinicOrderApplication.java
+++ src/main/java/com/epam/petclinic/order/PetClinicOrderApplication.java
@@ -2,13 +2,17 @@ package com.epam.petclinic.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableDiscoveryClient
 @EnableEurekaClient
 public class PetClinicOrderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetClinicOrderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicOrderApplication.class, args);
+    }
 }
